e4def72c741b03e6f524c2db1d05f3438b0a53e8
Juan Camacho <68569950+Juan321654@users.noreply.github.com>
2024-04-27 20:46:14 -0400

    BaseCounter done, and added animation when grabbing an object from the container

--- Assets/Scripts/BaseCounter.cs
+++ Assets/Scripts/BaseCounter.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
 
-public class BaseCounter : MonoBehaviour
+public class BaseCounter : MonoBehaviour, IKitchenObjectParent
 {
     [SerializeField] protected GameObject prefabToShowOnHighlight;
+    [SerializeField] private Transform counterTopPoint;
+    private KitchenObject kitchenObject;
     public virtual void Interact(Player player)
     {
         Debug.LogError("Interacting with BaseCounter class should not be possible. Please check the derived class.");
@@ -13,5 +15,30 @@ public class BaseCounter : MonoBehaviour
         if (prefabToShowOnHighlight == null) return;
 
         prefabToShowOnHighlight.SetActive(highlight);
+    }
+
+    public Transform GetKitchenObjectFollowTransform()
+    {
+        return counterTopPoint;
+    }
+
+    public void SetKitchenObject(KitchenObject kitchenObject)
+    {
+        this.kitchenObject = kitchenObject;
+    }
+
+    public KitchenObject GetKitchenObject()
+    {
+        return kitchenObject;
+    }
+
+    public void ClearKitchenObject()
+    {
+        kitchenObject = null;
+    }
+
+    public bool HasKitchenObject()
+    {
+        return kitchenObject != null;
     }
 }
--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -1,50 +1,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ClearCounter : BaseCounter, IKitchenObjectParent
+public class ClearCounter : BaseCounter
 {
 
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
-    [SerializeField] private Transform counterTopPoint;
-    // public GameObject prefabToShowOnHighlight;
-    private KitchenObject kitchenObject;
 
     public override void Interact(Player player)
     {
-        if (kitchenObject == null)
-        {
-            Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
-            kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(this);
-        }
-        else
-        {
-            kitchenObject.SetKitchenObjectParent(player);
-        }
+        
     }
-
-    public Transform GetKitchenObjectFollowTransform()
-    {
-        return counterTopPoint;
-    }
-
-    public void SetKitchenObject(KitchenObject kitchenObject)
-    {
-        this.kitchenObject = kitchenObject;
-    }
-
-    public KitchenObject GetKitchenObject()
-    {
-        return kitchenObject;
-    }
-
-    public void ClearKitchenObject()
-    {
-        kitchenObject = null;
-    }
-
-    public bool HasKitchenObject()
-    {
-        return kitchenObject != null;
-    }
-
 }
