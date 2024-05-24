2e54e1cd399f056ea5204d88d2057730204ced4a
Juan Camacho <68569950+Juan321654@users.noreply.github.com>
2024-05-23 23:39:22 -0400

    added UI for controls in options

--- Assets/Scripts/UI/OptionsUI.cs
+++ Assets/Scripts/UI/OptionsUI.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 public class OptionsUI : MonoBehaviour
@@ -12,8 +13,35 @@ public class OptionsUI : MonoBehaviour
     [SerializeField] private Button soundEffectsButton;
     [SerializeField] private Button musicButton;
     [SerializeField] private Button closeButton;
+    [SerializeField] private Button moveUpButton;
+    [SerializeField] private Button moveDownButton;
+    [SerializeField] private Button moveLeftButton;
+    [SerializeField] private Button moveRightButton;
+    [SerializeField] private Button interactButton;
+    [SerializeField] private Button interactAlternateButton;
+    [SerializeField] private Button pauseButton;
     [SerializeField] private TextMeshProUGUI soundEffectsText;
     [SerializeField] private TextMeshProUGUI musicText;
+    [SerializeField] private TextMeshProUGUI moveUpText;
+    [SerializeField] private TextMeshProUGUI moveDownText;
+    [SerializeField] private TextMeshProUGUI moveLeftText;
+    [SerializeField] private TextMeshProUGUI moveRightText;
+    [SerializeField] private TextMeshProUGUI interactText;
+    [SerializeField] private TextMeshProUGUI interactAlternateText;
+    [SerializeField] private TextMeshProUGUI pauseText;
+
+    private PlayerInputActions playerInputActions;
+
+    public enum Binding
+    {
+        MoveUp,
+        MoveDown,
+        MoveLeft,
+        MoveRight,
+        Interact,
+        InteractAlternate,
+        Pause
+    }
 
     private void Awake()
     {
@@ -32,6 +60,18 @@ public class OptionsUI : MonoBehaviour
         {
             Hide();
         });
+
+        playerInputActions = new PlayerInputActions();
+    }
+
+    private void OnEnable()
+    {
+        playerInputActions.Player.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerInputActions.Player.Disable();
     }
 
     private void Start()
@@ -52,6 +92,14 @@ public class OptionsUI : MonoBehaviour
     {
         soundEffectsText.text = $"Sound Effects: {Mathf.Round(SoundManager.Instance.GetVolume() * 10f)}";
         musicText.text = $"Music: {Mathf.Round(MusicManager.Instance.GetVolume() * 10f)}";
+
+        moveUpText.text = GetBindingText(Binding.MoveUp);
+        moveDownText.text = GetBindingText(Binding.MoveDown);
+        moveLeftText.text = GetBindingText(Binding.MoveLeft);
+        moveRightText.text = GetBindingText(Binding.MoveRight);
+        interactText.text = GetBindingText(Binding.Interact);
+        interactAlternateText.text = GetBindingText(Binding.InteractAlternate);
+        pauseText.text = GetBindingText(Binding.Pause);
     }
 
     public void Show()
@@ -62,5 +110,28 @@ public class OptionsUI : MonoBehaviour
     public void Hide()
     {
         gameObject.SetActive(false);
+    }
+
+    private string GetBindingText(Binding binding)
+    {
+        switch (binding)
+        {
+            case Binding.MoveUp:
+                return playerInputActions.Player.Move.bindings[1].ToDisplayString();
+            case Binding.MoveDown:
+                return playerInputActions.Player.Move.bindings[2].ToDisplayString();
+            case Binding.MoveLeft:
+                return playerInputActions.Player.Move.bindings[3].ToDisplayString();
+            case Binding.MoveRight:
+                return playerInputActions.Player.Move.bindings[4].ToDisplayString();
+            case Binding.Interact:
+                return playerInputActions.Player.Interact.bindings[0].ToDisplayString();
+            case Binding.InteractAlternate:
+                return playerInputActions.Player.InteractAlternate.bindings[0].ToDisplayString();
+            case Binding.Pause:
+                return playerInputActions.Player.Pause.bindings[0].ToDisplayString();
+            default:
+                return "";
+        }
     }
 }
